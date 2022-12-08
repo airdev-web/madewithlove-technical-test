d07d0606ac782cbae301916d4a1f8b8b6fa71399
Romain Vause <romain.vause@gmail.com>
2022-12-08 15:13:29 +0100

    Created the product page

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -9,6 +9,7 @@ class ProductSeeder extends Seeder
 {
     public function run()
     {
+        // Read the sample product json file and fill it in db
         $json_products = collect(json_decode(file_get_contents(resource_path('data/products.json')), true));
         $json_products->each(fn($product) => Product::create($product));
     }
