620fa5546583be8524a45c67d23c6cd1422db4c2
Romain Vause <romain.vause@gmail.com>
2022-12-08 14:04:26 +0100

    Created Product Model

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->words(3, true),
+            'price' => $this->faker->randomFloat(2, 10, 200),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ];
+    }
+}
