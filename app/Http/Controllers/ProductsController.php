f25ccde280cefa2fa5454b041d0fb0a961f83297
Romain Vause <romain.vause@gmail.com>
2022-12-09 15:40:08 +0100

    Add page to see the removed product from order

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -14,8 +14,10 @@ class ProductsController extends Controller
 
     public function removed_products()
     {
+        // We get the only product that was at leasted removed once
         $removed_product_ids = OrderProduct::where('removed', true)->groupBy('product_id')->get('product_id')->pluck('product_id');
 
+        // Create list of the products with the related order only when these where removed
         $products = Product::with(['order_products' => function($query) {
             $query->where('removed', true);
         }, 'order_products.order'])->whereIn('id', $removed_product_ids)->get();
